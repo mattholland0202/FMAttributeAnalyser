b8973a5ffc1a1f902a7b6772048c6f091f136b67
Matt Holland <mattholland0202@gmail.com>
2016-12-27 16:27:14 +0000

    Project creation, DI working, API started

---
+++ src/FMAA.Web.Api/Controllers/PlayersController.cs
@@ -0,0 +1,40 @@
+﻿using FMAA.BLL.Interfaces;
+using FMAA.Web.Api.Models.Return;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace FMAA.Web.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class PlayersController : Controller
+    {
+        private readonly IBLL bll;
+
+        public PlayersController(IBLL bll)
+        {
+            this.bll = bll;
+        }
+
+        /// <summary>
+        /// Get a collection of all Players
+        /// </summary>
+        /// <response code="200">A collection of Players is returned</response>
+        /// <response code="500">An error occurred</response>
+        [HttpGet]
+        [ProducesResponseType(typeof(List<PlayerModel>), StatusCodes.Status200OK)]
+        public IActionResult Get()
+        {
+            try
+            {
+                var players = bll.Players.GetAll();
+                return Ok(players);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex);
+            }
+        }
+    }
+}
---
+++ src/FMAA.Web.Api/Startup.cs
@@ -0,0 +1,47 @@
+﻿using FMAA.BLL.Interfaces;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.PlatformAbstractions;
+using System.IO;
+
+namespace FMAA.Web.Api
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+            services.AddSwaggerGen(options =>
+            {
+                options.IncludeXmlComments(GetXmlCommentsPath());
+                options.DescribeAllEnumsAsStrings();
+            });
+
+            services.AddScoped<IBLL, BLL.BLL>();
+        }
+
+        private string GetXmlCommentsPath()
+        {
+            var app = PlatformServices.Default.Application;
+            return Path.Combine(app.ApplicationBasePath, $"{app.ApplicationName}.xml");
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.UseStaticFiles();
+            app.UseDeveloperExceptionPage();
+            app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUi();
+        }
+    }
+}
